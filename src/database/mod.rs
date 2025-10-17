170ee364ed77569754b48df03dbfaafb416bdcec
Sudo Dios <sudodios@gmail.com>
2025-10-17 23:44:52 +0330

    Gate databases behind features #28

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -5,15 +5,21 @@ use tokio::sync::Mutex;
 use uuid::Uuid;
 use crate::config::SERVER_CONFIG;
 use crate::database::memory::MemoryDB;
+#[cfg(feature = "mysql")]
 use crate::database::mysql::MySql;
 use crate::database::none::NoneDB;
+#[cfg(feature = "postgres")]
 use crate::database::postgres::Postgres;
+#[cfg(feature = "sqlite")]
 use crate::database::sqlite::SQLite;
 use crate::results::TelemetryData;
 
+#[cfg(feature = "mysql")]
 mod mysql;
 mod none;
+#[cfg(feature = "postgres")]
 mod postgres;
+#[cfg(feature = "sqlite")]
 mod sqlite;
 mod memory;
 
@@ -34,16 +40,19 @@ pub fn generate_uuid () -> String {
 pub fn init () -> std::io::Result<Arc<Mutex<dyn Database + Send>>> {
     let config = SERVER_CONFIG.get().unwrap();
     match config.database_type.as_str() {
+        #[cfg(feature = "mysql")]
         "mysql" => {
             let mysql_setup = mysql::init(&config.database_username,&config.database_password,&config.database_hostname,&config.database_name)?;
             info!("Database {} initialized successfully","Mysql");
             Ok(Arc::new(Mutex::new(MySql{connection : mysql_setup})))
         }
+        #[cfg(feature = "postgres")]
         "postgres" => {
             let postgres_setup = postgres::init(&config.database_username,&config.database_password,&config.database_hostname,&config.database_name)?;
             info!("Database {} initialized successfully","Postgres");
             Ok(Arc::new(Mutex::new(Postgres {connection : postgres_setup})))
         }
+        #[cfg(feature = "sqlite")]
         "sqlite" => {
             let sqlite_setup = sqlite::init(&config.database_file)?;
             info!("Database {} initialized successfully","Sqlite");
